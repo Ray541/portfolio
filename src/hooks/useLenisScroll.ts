3366b0b11370db905d87a9b76b58bccafbd939db
pranavrao <pranav@geopageconsultants.in>
2025-07-24 13:47:39 +0530

    update: reduce lenis scroll duration

--- src/hooks/useLenisScroll.ts
+++ src/hooks/useLenisScroll.ts
@@ -10,7 +10,7 @@ export const useLenisScroll = () => {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 2.5,
+      duration: 2,
       easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
 
