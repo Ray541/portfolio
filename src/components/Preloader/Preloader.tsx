aeb58a480b6c462f07287cbba2460d77ee4612ba
pranavrao <pranav@geopageconsultants.in>
2025-08-13 14:58:53 +0530

    update: add name and change counter position

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -11,30 +11,36 @@ const Preloader = ({ onComplete }: PreloaderProps) => {
   const [fadeOut, setFadeOut] = useState(false);
   const [isDone, setIsDone] = useState(false);
 
-  useGSAP(() => {
-    // Scroll To Top
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  const percentageObj = { value: 0 };
 
-    const percentageObj = { value: 0 };
+  const updateCounter = () => {
+    setPercentage(Math.round(percentageObj.value));
+  };
 
-    const timeline = gsap.timeline();
-    timeline
-      .to({}, { duration: 0.7 }) // Initial delay
-      .to(percentageObj, {
-        value: 100,
-        duration: 1,
-        onUpdate: () => setPercentage(Math.round(percentageObj.value)),
-      })
-      .add(() => {
-        // Trigger fade out class Tailwind transition
-        setFadeOut(true);
-        // Wait for the CSS transition to finish, then unmount
-        setTimeout(() => {
-          setIsDone(true);
-          onComplete();
-        }, 700);
-      });
-  }, []);
+  useGSAP(
+    () => {
+      const timeline = gsap.timeline();
+
+      timeline
+        .to({}, { duration: 0.7 }) // Initial delay
+        .to(percentageObj, {
+          value: 100,
+          duration: 2,
+          ease: "power1.out",
+          onUpdate: updateCounter,
+        })
+        .then(() => {
+          // Trigger fade out class Tailwind transition
+          setFadeOut(true);
+          // Wait for the CSS transition to finish, then unmount
+          setTimeout(() => {
+            setIsDone(true);
+            onComplete();
+          }, 700);
+        });
+    },
+    { scope: containerRef }
+  );
 
   if (isDone) return null;
 
@@ -42,11 +48,18 @@ const Preloader = ({ onComplete }: PreloaderProps) => {
     <div
       ref={containerRef}
       className={cn(
-        "preloader-bg-grid-line fixed w-full min-h-screen z-10 flex items-center justify-center bg-background text-foreground transition-opacity duration-700",
+        "preloader preloader-bg-grid-line fixed w-full h-screen z-10 flex items-center justify-center bg-background text-foreground transition-opacity duration-300",
         fadeOut && "opacity-0"
       )}
     >
-      <span className="text-9xl font-black">{percentage}%</span>
+      <div className={cn("relative w-full h-full flex items-center justify-center")}>
+        <span className="text-5xl md:text-8xl lg:text-9xl font-black tracking-wide underline underline-offset-4">
+          Pranav Rao.
+        </span>
+        <span className="absolute bottom-20 right-20 text-4xl md:text-5xl lg:text-6xl">
+          {percentage}%
+        </span>
+      </div>
     </div>
   );
 };
