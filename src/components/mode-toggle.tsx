877147399710e10974e3ded55f0ed94b834178fa
Pranav Rao <pranavrao541@gmail.com>
2025-09-01 16:26:52 +0530

    update: change icon according to theme

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from "../hooks/use-theme";
 import { Button } from "@/components/ui/button";
-import { Moon, Sun, Laptop } from "lucide-react";
+import { Moon, Sun, Laptop2 } from "lucide-react";
 import { handleCursorEnter, handleCursorLeave } from "@/utils/gsapUtils";
 
 type ModeToggleProps = {
@@ -11,19 +11,22 @@ export function ModeToggle({ className }: ModeToggleProps) {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("system");
-    } else if (theme === "system") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
+    if (theme === "light") setTheme("dark");
+    else if (theme === "dark") setTheme("system");
+    else setTheme("light");
   };
 
-  const getIcon = () => {
-    if (theme === "dark") return <Sun />;
-    if (theme === "light") return <Moon />;
-    return <Laptop />;
+  const getIcon = (theme: string) => {
+    switch (theme) {
+      case "light":
+        return <Moon />;
+      case "dark":
+        return <Laptop2 />;
+      case "system":
+        return <Sun />;
+      default:
+        return <Sun />;
+    }
   };
 
   return (
@@ -35,7 +38,7 @@ export function ModeToggle({ className }: ModeToggleProps) {
       onMouseEnter={() => handleCursorEnter(2)}
       onMouseLeave={handleCursorLeave}
     >
-      {getIcon()}
+      {getIcon(theme)}
     </Button>
   );
 }
