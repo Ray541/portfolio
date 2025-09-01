8291446fa6a4699f54d9fe6e4ade6b38361e3021
Pranav Rao <pranavrao541@gmail.com>
2025-09-01 17:03:35 +0530

    feat: add tile animation

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -6,10 +6,12 @@ import { cn } from "@/lib/utils";
 type PreloaderProps = { onComplete: () => void };
 
 const Preloader = ({ onComplete }: PreloaderProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef(null);
+  const percentageTextRef = useRef<HTMLSpanElement | null>(null);
   const [percentage, setPercentage] = useState(0);
   const [fadeOut, setFadeOut] = useState(false);
   const [isDone, setIsDone] = useState(false);
+  const [isForeground, setIsForeground] = useState(false);
 
   const percentageObj = { value: 0 };
 
@@ -19,40 +21,72 @@ const Preloader = ({ onComplete }: PreloaderProps) => {
 
   useGSAP(
     () => {
-      const timeline = gsap.timeline();
+      const tl = gsap.timeline();
 
-      timeline
-        .to({}, { duration: 0.7 }) // Initial delay
-        .to(percentageObj, {
-          value: 100,
-          duration: 2,
-          ease: "power1.out",
-          onUpdate: updateCounter,
-        })
-        .then(() => {
-          // Trigger fade out class Tailwind transition
-          setFadeOut(true);
-          // Wait for the CSS transition to finish, then unmount
-          setTimeout(() => {
-            setIsDone(true);
-            onComplete();
-          }, 700);
-        });
+      tl.from(".tile", {
+        height: 0,
+        stagger: {
+          amount: -0.5,
+        },
+      });
+
+      tl.to(".tile", {
+        y: "100%",
+        stagger: {
+          amount: -1,
+        },
+        onStart: () => setIsForeground(!isForeground),
+      });
     },
     { scope: containerRef }
   );
 
+  useGSAP(() => {
+    const timeline = gsap.timeline();
+
+    timeline
+      .to({}, { duration: 0.7 }) // Initial delay
+      .to(percentageObj, {
+        value: 100,
+        duration: 2,
+        ease: "power1.inOut",
+        onUpdate: updateCounter,
+      })
+      .then(() => {
+        // Trigger fade out class Tailwind transition
+        setFadeOut(true);
+        // Wait for the CSS transition to finish, then unmount
+        setTimeout(() => {
+          setIsDone(true);
+          onComplete();
+        }, 300);
+      });
+  });
+
   if (isDone) return null;
 
   return (
-    <div
-      ref={containerRef}
-      className={cn(
-        "preloader relative w-full max-h-screen h-screen z-10 flex items-center justify-center text-foreground transition-opacity duration-300",
-        fadeOut && "opacity-0"
-      )}
-    >
-      <span className="font-black text-9xl">{percentage}%</span>
+    <div ref={containerRef} className="w-full min-h-screen flex relative overflow-hidden">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <div key={index} className="tile w-1/5 h-screen bg-foreground" />
+      ))}
+
+      <div
+        className={cn(
+          "absolute inset-0 flex items-center justify-center z-10 transition-all duration-300",
+          fadeOut && "opacity-0"
+        )}
+      >
+        <span
+          ref={percentageTextRef}
+          className={cn(
+            "font-black text-8xl sm:text-9xl transition-colors duration-700",
+            isForeground ? "text-foreground" : "text-background"
+          )}
+        >
+          {percentage}%
+        </span>
+      </div>
     </div>
   );
 };
