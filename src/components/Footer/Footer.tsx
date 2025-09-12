603b36ea746f80f3d0f550547b683ac95dd2d99f
pranavrao <pranav@geopageconsultants.in>
2025-09-12 13:08:15 +0530

    feat: add scroll to top function on name click

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,6 +7,8 @@ const CURRENT_YEAR = new Date().getFullYear();
 const Footer = () => {
   const [time, setTime] = useState(new Date());
 
+  const handleNameClick = () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000);
     return () => clearInterval(interval);
@@ -21,11 +23,11 @@ const Footer = () => {
       hour12: true,
     });
 
-    const dateString = time.toLocaleDateString("en-GB", {
+    const dateString = time.toLocaleDateString("en-US", {
       day: "2-digit",
-      month: "short",
+      month: "long",
       weekday: "long",
-    }); // e.g., 20-May-2025, Monday
+    }); // e.g., 20-May, Monday
 
     return (
       <div className="flex flex-row gap-5 md:gap-3 flex-wrap items-center text-center text-primary px-4 py-2">
@@ -34,7 +36,7 @@ const Footer = () => {
         </div>
         <div
           className="text-sm text-muted-foreground font-bold"
-          onMouseEnter={() => handleCursorEnter(2)}
+          onMouseEnter={() => handleCursorEnter(2.5)}
           onMouseLeave={handleCursorLeave}
         >
           {dateString}
@@ -46,22 +48,20 @@ const Footer = () => {
   return (
     <footer className="bg-background py-7 px-3 text-center text-muted-foreground border-t border-border border-dashed">
       <div className="flex flex-col md:flex-row gap-2 justify-between items-center max-w-5xl mx-auto">
-        <p className="text-sm font-medium flex items-center gap-1 tracking-wide">
-          Developed by{" "}
-          <span
-            className="text-lg text-primary font-black"
-            onMouseEnter={() => handleCursorEnter(2)}
-            onMouseLeave={handleCursorLeave}
-          >
-            {AUTHOR_NAME}
-          </span>
-        </p>
+        <span
+          className="text-lg text-primary font-black tracking-wide"
+          onMouseEnter={() => handleCursorEnter(2)}
+          onMouseLeave={handleCursorLeave}
+          onClick={handleNameClick}
+        >
+          {AUTHOR_NAME}
+        </span>
 
         <DigitalClock time={time} />
 
         <p
           className="text-md font-black"
-          onMouseEnter={() => handleCursorEnter(2.5)}
+          onMouseEnter={() => handleCursorEnter(2)}
           onMouseLeave={handleCursorLeave}
         >
           &copy; <span className="text-primary">{CURRENT_YEAR}</span>
