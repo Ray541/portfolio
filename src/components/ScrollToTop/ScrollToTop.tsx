f3c4f449d4c11f631481528ce34ec828f17c3443
Pranav Rao <pranavrao541@gmail.com>
2025-10-31 21:07:51 +0530

    update: remove gsap animation

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { MdArrowUpward } from "react-icons/md";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import { handleCursorEnter, handleCursorLeave } from "@/utils/gsapUtils";
 
 type ScrollToTopProps = {
@@ -11,7 +9,6 @@ type ScrollToTopProps = {
 
 const ScrollToTop = ({ className }: ScrollToTopProps) => {
   const [isVisible, setIsVisible] = useState(false);
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
 
   // Scroll listener
   useEffect(() => {
@@ -23,41 +20,21 @@ const ScrollToTop = ({ className }: ScrollToTopProps) => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  useGSAP(
-    () => {
-      if (isVisible) {
-        gsap.to(buttonRef.current, {
-          opacity: 1,
-          y: 0,
-          duration: 0.5,
-          pointerEvents: "auto",
-        });
-      } else {
-        gsap.to(buttonRef.current, {
-          opacity: 0,
-          y: 5,
-          duration: 0.5,
-          pointerEvents: "none",
-        });
-      }
-    },
-    { dependencies: [isVisible] }
-  );
-
   const scrollToTop = () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 
   return (
-    <Button
-      ref={buttonRef}
-      variant="default"
-      size="icon"
-      className={`opacity-0 pointer-events-none ${className}`}
-      onClick={scrollToTop}
-      onMouseEnter={() => handleCursorEnter(2)}
-      onMouseLeave={handleCursorLeave}
-    >
-      <MdArrowUpward />
-    </Button>
+    isVisible && (
+      <Button
+        variant="default"
+        size="icon"
+        className={`pointer-events-none ${className}`}
+        onClick={scrollToTop}
+        onMouseEnter={() => handleCursorEnter(2)}
+        onMouseLeave={handleCursorLeave}
+      >
+        <MdArrowUpward />
+      </Button>
+    )
   );
 };
 
