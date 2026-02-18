f582d6fdd634f13da3bc4a9aad1148b092e93d2a
Ray541 <pranavrao541@gmail.com>
2026-02-18 16:39:10 +0530

    refactor: change from gsap to motion

--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -1,34 +1,19 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
+import { motion, useScroll } from "motion/react";
 
-const ProgressBar = () => {
-  const progressRef = useRef<HTMLDivElement>(null);
+interface ProgressBarProps {
+  origin?: string;
+}
 
-  useGSAP(() => {
-    const updateProgress = () => {
-      const scrollTop = window.scrollY;
-      const docHeight = document.body.scrollHeight - window.innerHeight;
-      const progress = (scrollTop / docHeight) * 100;
-
-      gsap.to(progressRef.current, {
-        width: `${progress}%`,
-        duration: 0.2,
-        ease: "power1.out",
-      });
-    };
-
-    window.addEventListener("scroll", updateProgress);
-
-    return () => {
-      window.removeEventListener("scroll", updateProgress);
-    };
-  }, []);
+const ProgressBar = ({ origin = "left" }: ProgressBarProps) => {
+  const { scrollYProgress } = useScroll();
 
   return (
-    <div className="fixed top-0 left-0 w-full h-1 bg-transparent z-5">
-      <div ref={progressRef} className="h-full bg-foreground origin-left" style={{ width: "0%" }} />
-    </div>
+    <motion.div
+      className={`h-1 bg-foreground fixed top-0 right-0 left-0 origin-${origin}`}
+      style={{
+        scaleX: scrollYProgress,
+      }}
+    />
   );
 };
 
