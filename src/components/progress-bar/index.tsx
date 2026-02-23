2e1249da05f198993914ace437b0f9264a22da90
Pranav Rao <pranavrao541@gmail.com>
2026-02-23 16:47:38 +0530

    feat: add progress bar with bg varient

--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -1,19 +1,40 @@
 import { motion, useScroll } from "motion/react";
 
 interface ProgressBarProps {
+  className?: string;
   origin?: string;
+  withBg?: boolean;
+  bgClassName?: string;
+  barClassName?: string;
 }
 
-const ProgressBar = ({ origin = "left" }: ProgressBarProps) => {
+const ProgressBar = ({
+  className = "h-0.5 bg-foreground fixed inset-0 z-50",
+  origin = "left",
+  withBg = false,
+  bgClassName = "h-0.5 bg-primary/20 fixed inset-0 z-50",
+  barClassName = "h-0.5 bg-foreground",
+}: ProgressBarProps) => {
   const { scrollYProgress } = useScroll();
 
-  return (
+  return !withBg ? (
     <motion.div
-      className={`h-1 bg-foreground fixed top-0 right-0 left-0 origin-${origin}`}
+      className={className}
       style={{
         scaleX: scrollYProgress,
+        transformOrigin: origin,
       }}
     />
+  ) : (
+    <div className={bgClassName}>
+      <motion.div
+        className={barClassName}
+        style={{
+          scaleX: scrollYProgress,
+          transformOrigin: origin,
+        }}
+      ></motion.div>
+    </div>
   );
 };
 
