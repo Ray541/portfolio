1c3f87324647f6d6267eda89d61d28b4765e52ca
Pranav Rao <pranavrao541@gmail.com>
2026-02-23 23:45:29 +0530

    update: change ui, styles and animations

--- src/sections/home/index.tsx
+++ src/sections/home/index.tsx
@@ -5,9 +5,44 @@ import { Button } from "@/components/ui/button";
 import Section from "@/components/section";
 import SocialButton from "@/components/social-button";
 import AlternateText from "@/components/animations/text-animations/AlternateText";
-import { motion } from "motion/react";
+import { handleCursorEnter, handleCursorLeave } from "@/utils/cursorUtils";
+import { motion, Variants } from "motion/react";
 
 const MotionButton = motion.create(Button);
+
+const containerVariants = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.15, // controls delay between children
+      delayChildren: 0.2,
+    },
+  },
+};
+
+const itemVariants: Variants = {
+  hidden: { y: 20, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: "easeIn",
+    },
+  },
+};
+
+const xItemVariants: Variants = {
+  hidden: { x: 20, opacity: 0 },
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: "easeIn",
+    },
+  },
+};
 
 const socialLinks = [
   {
@@ -35,68 +70,54 @@ const socialLinks = [
 const Home = () => {
   return (
     <Section sectionName="home" className="min-h-[100svh] lg:min-h-dvh overflow-x-hidden">
-      <div className="min-h-[100svh] lg:min-h-dvh flex flex-col items-center justify-center text-center lg:text-start gap-4 sm:gap-2 lg:w-3/4  p-3 md:p-0">
-        <motion.div
-          initial={{ y: "100%", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            ease: "easeIn",
-            type: "spring",
-            stiffness: 200,
-            damping: 15,
-          }}
-          className="flex items-baseline gap-1"
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+        className="min-h-[100svh] lg:min-h-dvh flex flex-col items-center justify-center lg:w-3/4 p-3 md:p-0"
+      >
+        <motion.h1
+          variants={itemVariants}
+          className="w-full text-center lg:text-start text-4xl md:text-5xl lg:text-8xl xl:pl-10 font-semibold text-foreground"
         >
-          <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-semibold text-foreground">
-            Hi, I'm
-          </h1>
+          Hi, I'm
+        </motion.h1>
+        <motion.div variants={itemVariants} className="w-full text-center lg:text-end">
           <AlternateText
             initialText="Pranav Rao."
             altText="Web Dev IN."
-            initialTextClassName="font-extrabold text-muted text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl"
-            altTextClassName="font-extrabold text-muted text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl"
+            // display="block"
+            initialTextClassName="w-full font-extrabold text-foreground text-4xl sm:text-5xl md:text-7xl lg:text-9xl"
+            altTextClassName="w-full font-extrabold text-foreground text-4xl sm:text-5xl md:text-7xl lg:text-9xl"
+            onMouseEnter={() => handleCursorEnter(12)}
+            onMouseLeave={() => handleCursorLeave()}
           />
         </motion.div>
 
         <motion.p
-          initial={{ y: "100%", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{
-            duration: 0.5,
-            ease: "easeIn",
-            type: "spring",
-            stiffness: 200,
-            damping: 15,
-          }}
-          className="text-center font-medium text-muted-foreground leading-relaxed"
+          variants={itemVariants}
+          className="text-center text-muted-foreground leading-relaxed my-2"
         >
-          Passionate{" "}
-          <span className="font-light text-lg lg:text-xl xl:text-2xl">Frontend Developer</span>{" "}
-          started my career in Web Dev. I specialize in using{" "}
-          <span className="font-light text-lg lg:text-xl xl:text-2xl">React.js</span> to build
-          modern, dynamic and efficient websites.
+          2 years of experience developing modern web interfaces with{" "}
+          <span
+            className="font-light text-lg lg:text-xl xl:text-3xl"
+            onMouseEnter={() => handleCursorEnter(3)}
+            onMouseLeave={() => handleCursorLeave()}
+          >
+            React.js
+          </span>
+          , building scalable solutions that prioritize usability and performance.
         </motion.p>
 
         <MotionButton
+          variants={itemVariants}
           variant="default"
-          className="gap-2 mt-2 transition-colors"
+          className="gap-2 my-2 transition-colors"
           onClick={() => window.open(resume, "_blank", "noopener,noreferrer")}
-          initial={{ y: "100%", opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          whileHover={{
-            y: -2.5,
-          }}
+          onMouseEnter={() => handleCursorEnter(2)}
+          onMouseLeave={() => handleCursorLeave()}
           whileTap={{
-            y: 0,
             scale: 0.95,
-          }}
-          transition={{
-            duration: 0.5,
-            ease: "easeIn",
-            type: "spring",
-            stiffness: 200,
-            damping: 15,
           }}
         >
           <MdOutlineFileDownload />
@@ -105,17 +126,10 @@ const Home = () => {
 
         <div className="flex flex-col items-center justify-center gap-5">
           <motion.div
+            variants={xItemVariants}
             className="hidden lg:flex lg:gap-2 absolute right-5 top-1/2 transform -translate-y-1/2 z-5 flex-col items-center before:content-[''] before:w-px before:h-30 before:bg-border after:content-[''] after:w-px after:h-30 after:bg-border"
-            initial={{ x: "100%", opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{
-              delay: 0.5,
-              duration: 0.5,
-              ease: "easeIn",
-              type: "spring",
-              stiffness: 200,
-              damping: 15,
-            }}
+            onMouseEnter={() => handleCursorEnter(3)}
+            onMouseLeave={() => handleCursorLeave()}
           >
             {socialLinks.map((social, index) => (
               <SocialButton
@@ -127,18 +141,7 @@ const Home = () => {
             ))}
           </motion.div>
 
-          <motion.div
-            className="flex gap-2 lg:hidden"
-            initial={{ y: "100%", opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              ease: "easeIn",
-              type: "spring",
-              stiffness: 200,
-              damping: 15,
-            }}
-          >
+          <motion.div variants={itemVariants} className="flex gap-2 lg:hidden">
             {socialLinks.map((social, index) => (
               <SocialButton
                 key={index}
@@ -149,7 +152,7 @@ const Home = () => {
             ))}
           </motion.div>
         </div>
-      </div>
+      </motion.div>
     </Section>
   );
 };
