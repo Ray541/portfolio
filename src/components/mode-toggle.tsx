15521369f0886d7f1b5912aa25eea4904593c4ac
Pranav Rao <pranavrao541@gmail.com>
2026-02-24 00:15:05 +0530

    update: add motion animation

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,10 +2,13 @@ import { useTheme } from "@/hooks/use-theme";
 import { Button } from "@/components/ui/button";
 import { Moon, Sun, Laptop2 } from "lucide-react";
 import { handleCursorEnter, handleCursorLeave } from "@/utils/cursorUtils";
+import { motion } from "motion/react";
 
 type ModeToggleProps = {
   className?: string;
 };
+
+const MotionButton = motion.create(Button);
 
 export function ModeToggle({ className }: ModeToggleProps) {
   const { theme, setTheme } = useTheme();
@@ -30,15 +33,21 @@ export function ModeToggle({ className }: ModeToggleProps) {
   };
 
   return (
-    <Button
+    <MotionButton
       variant="default"
       size="icon"
       className={className}
       onClick={toggleTheme}
       onMouseEnter={() => handleCursorEnter(2)}
       onMouseLeave={handleCursorLeave}
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.3, delay: 1 }}
+      whileTap={{
+        scale: 0.85,
+      }}
     >
       {getIcon(theme)}
-    </Button>
+    </MotionButton>
   );
 }
--- src/components/scroll-to-top/index.tsx
+++ src/components/scroll-to-top/index.tsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { MdArrowUpward } from "react-icons/md";
 import { handleCursorEnter, handleCursorLeave } from "@/utils/cursorUtils";
+import { motion, AnimatePresence } from "motion/react";
 
 type ScrollToTopProps = {
   className?: string;
 };
 
+const MotionButton = motion.create(Button);
+
 const ScrollToTop = ({ className }: ScrollToTopProps) => {
   const [isVisible, setIsVisible] = useState(false);
 
-  // Scroll listener
   useEffect(() => {
     const handleScroll = () => {
       setIsVisible(window.scrollY > 1000);
@@ -23,18 +25,28 @@ const ScrollToTop = ({ className }: ScrollToTopProps) => {
   const scrollToTop = () => window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 
   return (
-    isVisible && (
-      <Button
-        variant="default"
-        size="icon"
-        className={className}
-        onClick={scrollToTop}
-        onMouseEnter={() => handleCursorEnter(2)}
-        onMouseLeave={handleCursorLeave}
-      >
-        <MdArrowUpward />
-      </Button>
-    )
+    <AnimatePresence>
+      {isVisible && (
+        <MotionButton
+          key="scroll-top"
+          variant="default"
+          size="icon"
+          className={className}
+          onClick={scrollToTop}
+          onMouseEnter={() => handleCursorEnter(2)}
+          onMouseLeave={handleCursorLeave}
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: 10, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          whileTap={{
+            scale: 0.85,
+          }}
+        >
+          <MdArrowUpward />
+        </MotionButton>
+      )}
+    </AnimatePresence>
   );
 };
 
